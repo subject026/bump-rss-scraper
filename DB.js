dcea4916a137fd9c7e72a09b2e1ccdb7d0e71c9c
subject026 <sudobuildspeakers@gmail.com>
2020-07-08 22:13:18 +0100

    woop

---
+++ DB.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require("sequelize");
+
+const DB = () => {
+  return new Promise((resolve, reject) => {
+    let instance;
+    if (!instance) {
+      instance = new Sequelize({
+        dialect: 'sqlite',
+        storage: 'path/to/database.sqlite'
+      });
+      console.log(instance);
+        instance.authenticate().then(() => {
+          console.log("Connection has been established successfully.");
+          resolve(instance);
+        }).catch(err => {
+        console.error("Unable to connect to the database:", err);
+        reject();
+        });
+      }
+    }
+    resolve(instance);
+  })
+};
+
+module.exports = DB;
---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const DB = require("./DB");
+
+const init = async () => {
+  const SQL = await DB();
+
+  console.log(SQL);
+};
+
+init();
