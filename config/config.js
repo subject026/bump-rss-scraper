6ca4a5787dc2ebc326965d0be1c3d9efc6bf6bc0
subject026 <sudobuildspeakers@gmail.com>
2020-07-09 00:10:39 +0100

    config stuff

---
+++ config/config.js
@@ -0,0 +1,32 @@
+const makeConfig = () => {
+  switch (process.env.MODE) {
+    case 'development':
+      return {
+        dialect: 'postgresql',
+        username: process.env.DEV_DB_USERNAME,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DB_DATABASE,
+        host: process.env.DEV_DB_HOST
+      }
+    case 'test':
+      return {
+        username: 'root',
+        password: null,
+        database: 'database_test',
+        host: '127.0.0.1',
+        dialect: 'postgresql'
+      }
+    case 'production':
+      return {
+        username: 'root',
+        password: 'butterflywhenterrible',
+        database: 'database_production',
+        host: '127.0.0.1',
+        dialect: 'postgresql'
+      }
+    default:
+      return {}
+  }
+}
+
+module.exports = makeConfig()
